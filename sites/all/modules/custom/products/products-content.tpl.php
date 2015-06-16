af313fd9e1d90df6d3dbb6040f06bf8d3cfa70a9
e.dyukarev <e.dyukarev@itransition.com>
2015-06-16 20:42:29 +0300

    1

--- sites/all/modules/custom/products/products-content.tpl.php
+++ sites/all/modules/custom/products/products-content.tpl.php
@@ -1,5 +1,17 @@
-<?php foreach ($items->channel->item as $item): ?>
-    <span class="title"><a href="<?php echo $item->link; ?>"><?php echo $item->title; ?></a></span><br><br>
-    <?php echo $item->description; ?><br><br><?php echo $item->pubDate; ?>
-    <hr><br><br>
-<?php endforeach; ?>
+<span class="title">Name:
+    <?php echo $product['name']; ?>
+</span>
+<br><br>
+Info:
+<?php echo $product['info']; ?><br><br>
+Type:
+<?php echo $product['tp_name']; ?><br><br>
+<?php
+$image = variable_get('file_public_path', conf_path() . '/files') . '/' . file_uri_target($product['uri']); ?>
+<img style="height: 100px" src="../../../../../<?php echo $image; ?>"><br><br>
+Created:
+<?php echo $product['created_at']; ?><br><br>
+Updated:
+<?php echo $product['updated_at']; ?><br><br>
+<hr>
+<br><br>
